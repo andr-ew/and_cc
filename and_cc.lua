1c43b2687ac6ad509f78a90d38c2bbcdba03c273
andrew <andrewcshike@gmail.com>
2020-08-31 06:26:52 +0700

    led tuning

--- and_cc.lua
+++ and_cc.lua
@@ -1,12 +1,25 @@
+-- and
+
+-- util.scandir(_path.audio .. '/loops')
+
 subscript = include 'subscript/lib/subscript'
 cc = subscript 'cheat_codes'
 lm = include 'ledmap/lib/ledmap'
 
 function init()
     params:add_separator("and_cc")
-        
-
-    lm:map(g, '2011')
+    
+    for i=1,3 do
+        params:add{
+            type="trigger",
+            id='randloop_' .. tostring(i),
+            action = function()
+                
+            end
+        }
+    end    
+ 
+    lm:map(g, { 0, 4, 0, 4, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 })
 
     cc.init()    
 end
