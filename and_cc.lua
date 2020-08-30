59828e545cb26567406de2dde9611b14e29f6adc
andrew <andrewcshike@gmail.com>
2020-08-30 07:39:43 +0700

    subscript

--- and_cc.lua
+++ and_cc.lua
@@ -1,1 +1,30 @@
-include 'cheat_codes'
+subscript = include 'subscript/lib/subscript'
+cc = subscript 'cheat_codes'
+lm = include 'ledmap/lib/ledmap'
+
+function init()
+    params:add_separator("and_cc")
+        
+
+    lm:map(g, '2011')
+
+    cc.init()    
+end
+
+function key(n,z)
+    cc.key(n,z)
+end
+
+function enc(n,d)
+    cc.enc(n,d)
+end
+
+function redraw()
+    cc.redraw()
+end
+
+function cleanup()
+    cc.cleanup()
+
+    lm:unmap(g)
+end
